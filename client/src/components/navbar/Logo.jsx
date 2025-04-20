b13b93d6adfa34b9c1ba50c881d2b1512beded46
khanmatin900 <matimkhan480@gmail.com>
2025-04-20 13:16:48 +0530

    fix navbar

---
+++ client/src/components/navbar/Logo.jsx
@@ -0,0 +1,12 @@
+// src/components/Logo.jsx
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="text-2xl font-bold text-white transform hover:scale-105 transition-all duration-300">
+      <a href="/">Shop.com</a>
+    </div>
+  );
+};
+
+export default Logo;
